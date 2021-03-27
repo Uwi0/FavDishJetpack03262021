25f851dc3f452a5abbd07017582d70c4c8fbc5de
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-03-27 19:03:57 +0700

    splash screen preparation

---
+++ app/src/main/java/com/kakapo/favoritdish/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.kakapo.favoritdish.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
