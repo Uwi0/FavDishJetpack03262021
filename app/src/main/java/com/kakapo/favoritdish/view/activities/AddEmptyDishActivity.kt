b55ffda2cc5cacbeb5eaaa3937b2bcd54b706849
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-03-28 13:45:43 +0700

    add activityAddDishs done

---
+++ app/src/main/java/com/kakapo/favoritdish/view/activities/AddEmptyDishActivity.kt
@@ -0,0 +1,12 @@
+package com.kakapo.favoritdish.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kakapo.favoritdish.R
+
+class AddEmptyDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_empty_dish)
+    }
+}
