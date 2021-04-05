6f46c6172ac0160a96a03e17ea4832ed3fd1d964
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-05 15:48:26 +0700

    view selesai

--- app/src/main/java/com/kakapo/favoritdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/activities/AddUpdateDishActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kakapo.favoritdish.R
 
-class AddEmptyDishActivity : AppCompatActivity() {
+class AddUpdateDishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_empty_dish)
+        setContentView(R.layout.activity_add_update_dish)
     }
 }
