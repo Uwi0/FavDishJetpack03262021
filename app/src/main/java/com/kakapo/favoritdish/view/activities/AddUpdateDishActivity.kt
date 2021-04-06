d691341611d4d0e9defa6d8aab92e7d0ee85cc1c
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-06 08:55:01 +0700

    permissions

--- app/src/main/java/com/kakapo/favoritdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/activities/AddUpdateDishActivity.kt
@@ -1,13 +1,25 @@
 package com.kakapo.favoritdish.view.activities
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import com.kakapo.favoritdish.R
 import com.kakapo.favoritdish.databinding.ActivityAddUpdateDishBinding
 import com.kakapo.favoritdish.databinding.DialogCustomImageSelectionBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -43,20 +55,60 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         dialog.setContentView(binding.root)
 
         binding.tvCamera.setOnClickListener{
-            Toast.makeText(
-                this,
-                "camera clicked",
-                Toast.LENGTH_SHORT
-            ).show()
+            Dexter.withContext(this).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(
+                            this@AddUpdateDishActivity,
+                            "You have permission now",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }else{
+                        showRationalDialogForPermission()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                   showRationalDialogForPermission()
+                }
+
+            }).onSameThread().check()
             dialog.dismiss()
         }
 
         binding.tvGallery.setOnClickListener{
-            Toast.makeText(
-                this,
-                "Gallery CLicked",
-                Toast.LENGTH_SHORT
-            ).show()
+            Dexter.withContext(this@AddUpdateDishActivity).withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if(report!!.areAllPermissionsGranted()){
+                        Toast.makeText(
+                            this@AddUpdateDishActivity,
+                            "You have permission now",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }else{
+                        showRationalDialogForPermission()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permission: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermission()
+                }
+
+            }).onSameThread().check()
+
             dialog.dismiss()
         }
         dialog.show()
@@ -68,5 +120,26 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         mBinding.toolbarAddDishActivity.setNavigationOnClickListener{
             onBackPressed()
         }
+    }
+
+    private fun showRationalDialogForPermission(){
+        AlertDialog.Builder(this).setMessage("It Looks like you have turned off permissions" +
+                " required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton("GO TO SETTINGS"){ _,_ ->
+                try{
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    Log.e("Error permission", "error give permissions AddUpdateActivity " +
+                            "shownRationalDialogPermission")
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){dialog,_ ->
+                dialog.dismiss()
+            }
+            .show()
     }
 }
