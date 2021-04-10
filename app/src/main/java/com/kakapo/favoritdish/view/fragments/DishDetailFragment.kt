424217a25882020e81a6805ad021c3162c44bc3d
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-11 06:18:10 +0700

    fragments details done

--- app/src/main/java/com/kakapo/favoritdish/view/fragments/DishDetailFragment.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/fragments/DishDetailFragment.kt
@@ -7,7 +7,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.kakapo.favoritdish.R
+import com.kakapo.favoritdish.databinding.FragmentDishDetailBinding
+import java.io.IOException
+import java.util.*
 
 
 /**
@@ -16,6 +20,8 @@ import com.kakapo.favoritdish.R
  * create an instance of this fragment.
  */
 class DishDetailFragment : Fragment() {
+
+    private var mBinding: FragmentDishDetailBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +32,39 @@ class DishDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dish_detail, container, false)
+        mBinding = FragmentDishDetailBinding.inflate(inflater, container, false)
+        return mBinding!!.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailFragmentArgs by navArgs()
-        Log.i("Dish Title", args.dishDetails.title)
+
+        args.let{
+            try{
+                Glide.with(requireActivity())
+                    .load(it.dishDetails.image)
+                    .centerCrop()
+                    .into(mBinding!!.ivDishImage)
+            }catch (e: IOException){
+                Log.e("error load image", "error binding image from database", e)
+                e.printStackTrace()
+            }
+
+            mBinding!!.tvTitle.text = it.dishDetails.title
+            mBinding!!.tvType.text = it.dishDetails.type.capitalize(Locale.ROOT)
+            mBinding!!.tvCategory.text = it.dishDetails.category
+            mBinding!!.tvIngredients.text = it.dishDetails.ingredients
+            mBinding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+            mBinding!!.tvCookingTime.text =
+                resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
+        }
+
     }
 
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
+    }
 
 }
