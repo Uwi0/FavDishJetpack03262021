1f5f600b24ea92de554e88042c761e64a5c4795c
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-17 07:56:55 +0700

    populate data into ui

--- app/src/main/java/com/kakapo/favoritdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/fragments/RandomDishFragment.kt
@@ -1,15 +1,26 @@
 package com.kakapo.favoritdish.view.fragments
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.kakapo.favoritdish.R
+import com.kakapo.favoritdish.application.FavDishApplication
 import com.kakapo.favoritdish.databinding.FragmentRandomDishesBinding
+import com.kakapo.favoritdish.model.entities.FavDish
+import com.kakapo.favoritdish.model.entities.RandomDish
+import com.kakapo.favoritdish.utils.Constants
+import com.kakapo.favoritdish.viewmodel.FavDishViewModel
+import com.kakapo.favoritdish.viewmodel.FavDishViewModelFactory
 import com.kakapo.favoritdish.viewmodel.NotificationsViewModel
 import com.kakapo.favoritdish.viewmodel.RandomDishViewModel
 
@@ -46,6 +57,7 @@ class RandomDishFragment : Fragment() {
         mRandomDishViewModel.randomDishResponse.observe(viewLifecycleOwner, { randomDishResponse ->
             randomDishResponse?.let {
                 Log.i("Random Dish Response", "${randomDishResponse.recipes[0]}")
+                setRandomDishResponseInUi(randomDishResponse.recipes[0])
             }
         })
 
@@ -60,5 +72,82 @@ class RandomDishFragment : Fragment() {
                 Log.i("Random Dish Loading", "$loadRandomDish")
             }
         })
+    }
+
+    private fun setRandomDishResponseInUi(recipe: RandomDish.Recipe){
+        Glide.with(requireActivity())
+            .load(recipe.image)
+            .centerCrop()
+            .into(mBinding!!.ivDishImage)
+
+        mBinding!!.tvTitle.text = recipe.title
+        var dishType: String = "other"
+
+        if(recipe.dishTypes.isNotEmpty()){
+            dishType = recipe.dishTypes[0]
+            mBinding!!.tvType.text = dishType
+        }
+
+        mBinding!!.tvCategory.text = "other"
+        var ingredients = ""
+        for (value in recipe.extendedIngredients){
+            if(ingredients.isEmpty()){
+                ingredients = value.original
+            }else{
+                ingredients = ingredients + ", \n" + value.original
+            }
+        }
+
+        mBinding!!.tvIngredients.text = ingredients
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            mBinding!!.tvCookingDirection.text = Html.fromHtml(
+                recipe.instructions,
+                Html.FROM_HTML_MODE_COMPACT
+            )
+        }else{
+            @Suppress("DEPRECATION")
+            mBinding!!.tvCookingDirection.text = Html.fromHtml(recipe.instructions)
+        }
+
+        mBinding!!.tvCookingTime.text =  resources.getString(
+            R.string.lbl_estimate_cooking_time,
+            recipe.readyInMinutes.toString()
+        )
+
+        mBinding!!.ivFavoriteDish.setOnClickListener {
+            val randomDishDetails = FavDish(
+                recipe.image,
+                Constants.DISH_IMAGE_SOURCE_ONLINE,
+                recipe.title,
+                dishType,
+                "other",
+                ingredients,
+                recipe.readyInMinutes.toString(),
+                recipe.instructions,
+                true
+            )
+            val mFavDishViewModel: FavDishViewModel by viewModels {
+                FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+            }
+
+            mFavDishViewModel.insertDish(randomDishDetails)
+
+            mBinding!!.ivFavoriteDish.setImageDrawable(
+                ContextCompat.getDrawable(
+                    requireActivity(),
+                    R.drawable.ic_favorite_selected
+                )
+            )
+
+            Toast.makeText(
+                requireActivity(),
+                resources.getString(R.string.msg_added_to_favortes),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+
+
     }
 }
