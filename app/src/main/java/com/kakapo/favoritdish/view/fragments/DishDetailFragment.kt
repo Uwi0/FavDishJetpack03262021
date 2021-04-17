4cfd812f9f7a00f0ddc428eabf2660044d49f125
Uwi0 <72695018+Uwi0@users.noreply.github.com>
2021-04-17 13:53:26 +0700

    share content

--- app/src/main/java/com/kakapo/favoritdish/view/fragments/DishDetailFragment.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/fragments/DishDetailFragment.kt
@@ -1,12 +1,13 @@
 package com.kakapo.favoritdish.view.fragments
 
+import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.text.Html
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.toBitmap
@@ -21,6 +22,8 @@ import com.bumptech.glide.request.target.Target
 import com.kakapo.favoritdish.R
 import com.kakapo.favoritdish.application.FavDishApplication
 import com.kakapo.favoritdish.databinding.FragmentDishDetailBinding
+import com.kakapo.favoritdish.model.entities.FavDish
+import com.kakapo.favoritdish.utils.Constants
 import com.kakapo.favoritdish.viewmodel.FavDishViewModel
 import com.kakapo.favoritdish.viewmodel.FavDishViewModelFactory
 import java.io.IOException
@@ -29,12 +32,14 @@ import java.util.*
 class DishDetailFragment : Fragment() {
 
     private var mBinding: FragmentDishDetailBinding? = null
+    private var mFavDishDetails: FavDish? = null
     private val mFavDishViewModel: FavDishViewModel by viewModels{
         FavDishViewModelFactory(((requireActivity().application) as FavDishApplication).repository)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -49,6 +54,7 @@ class DishDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailFragmentArgs by navArgs()
+        mFavDishDetails = args.dishDetails
 
         args.let{
             try{
@@ -95,6 +101,17 @@ class DishDetailFragment : Fragment() {
             mBinding!!.tvCategory.text = it.dishDetails.category
             mBinding!!.tvIngredients.text = it.dishDetails.ingredients
             mBinding!!.tvCookingDirection.text = it.dishDetails.directionToCook
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                mBinding!!.tvCookingDirection.text = Html.fromHtml(
+                    it.dishDetails.directionToCook,
+                    Html.FROM_HTML_MODE_COMPACT
+                ).toString()
+            }else{
+                @Suppress("DEPRECATION")
+                mBinding!!.tvCookingDirection.text = Html.fromHtml(it.dishDetails.directionToCook).toString()
+            }
+
             mBinding!!.tvCookingTime.text =
                 resources.getString(R.string.lbl_estimate_cooking_time, it.dishDetails.cookingTime)
 
@@ -151,6 +168,55 @@ class DishDetailFragment : Fragment() {
     override fun onDestroy() {
         mBinding = null
         super.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_share, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_share_dish ->{
+                val type = "text/plain"
+                val subject = "Checkout this dish recipe"
+                var extraText = ""
+                val shareWith = "Share With"
+
+                mFavDishDetails?.let {
+                    var image = ""
+                    if (it.imageSource == Constants.DISH_IMAGE_SOURCE_ONLINE){
+                        image = it.image
+                    }
+                    var cookingInstruction = ""
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+                        cookingInstruction = Html.fromHtml(
+                            it.directionToCook,
+                            Html.FROM_HTML_MODE_COMPACT
+                        ).toString()
+                    }else{
+                        @Suppress("DEPRECATION")
+                        cookingInstruction = Html.fromHtml(it.directionToCook).toString()
+                    }
+
+                    extraText =
+                        "$image \n" +
+                        "\n Title:  ${it.title} \n\n Type: ${it.type} \n\n Category: ${it.category}" +
+                         "\n\n Ingredients: \n ${it.ingredients} \n\n Instructions To Cook: \n $cookingInstruction" +
+                         "\n\n Time required to cook the dish approx ${it.cookingTime} minutes."
+                }
+
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = type
+                intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+                intent.putExtra(Intent.EXTRA_TEXT, extraText)
+                startActivity(Intent.createChooser(intent,shareWith))
+
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
     }
 
 }
--- app/src/main/java/com/kakapo/favoritdish/view/fragments/RandomDishFragment.kt
+++ app/src/main/java/com/kakapo/favoritdish/view/fragments/RandomDishFragment.kt
@@ -1,5 +1,6 @@
 package com.kakapo.favoritdish.view.fragments
 
+import android.app.Dialog
 import android.os.Build
 import android.os.Bundle
 import android.text.Html
@@ -21,14 +22,13 @@ import com.kakapo.favoritdish.model.entities.RandomDish
 import com.kakapo.favoritdish.utils.Constants
 import com.kakapo.favoritdish.viewmodel.FavDishViewModel
 import com.kakapo.favoritdish.viewmodel.FavDishViewModelFactory
-import com.kakapo.favoritdish.viewmodel.NotificationsViewModel
 import com.kakapo.favoritdish.viewmodel.RandomDishViewModel
 
 class RandomDishFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
     private var mBinding: FragmentRandomDishesBinding? = null
     private lateinit var mRandomDishViewModel: RandomDishViewModel
+    private lateinit var mProgressDialog: Dialog
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,10 +42,14 @@ class RandomDishFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mRandomDishViewModel = ViewModelProvider(this).get(RandomDishViewModel::class.java)
-
+        mProgressDialog = Dialog(requireActivity())
         mRandomDishViewModel.getRandomRecipeFromAPI()
 
         randomDishViewModelObserver()
+
+        mBinding!!.srlRandomDish.setOnRefreshListener {
+            mRandomDishViewModel.getRandomRecipeFromAPI()
+        }
     }
 
     override fun onDestroyView() {
@@ -57,19 +61,33 @@ class RandomDishFragment : Fragment() {
         mRandomDishViewModel.randomDishResponse.observe(viewLifecycleOwner, { randomDishResponse ->
             randomDishResponse?.let {
                 Log.i("Random Dish Response", "${randomDishResponse.recipes[0]}")
+                if(mBinding!!.srlRandomDish.isRefreshing){
+                    mBinding!!.srlRandomDish.isRefreshing = false
+                }
                 setRandomDishResponseInUi(randomDishResponse.recipes[0])
+
             }
         })
 
         mRandomDishViewModel.randomDishLoadingError.observe(viewLifecycleOwner, {dataError ->
             dataError?.let{
                 Log.i("Random Dish Api Error", "$dataError")
+                if(mBinding!!.srlRandomDish.isRefreshing){
+                    mBinding!!.srlRandomDish.isRefreshing = false
+                }
+                if (!dataError){
+                    hideProgressDialog()
+                }
             }
         })
 
-        mRandomDishViewModel.loadRandomDish.observe(viewLifecycleOwner,{loadRandomDish ->
-            loadRandomDish?.let{
+        mRandomDishViewModel.loadRandomDish.observe(viewLifecycleOwner, { loadRandomDish ->
+            loadRandomDish?.let {
                 Log.i("Random Dish Loading", "$loadRandomDish")
+                if(loadRandomDish && !mBinding!!.srlRandomDish.isRefreshing) {
+                    showCustomProgressDialog()
+                }
+
             }
         })
     }
@@ -81,14 +99,14 @@ class RandomDishFragment : Fragment() {
             .into(mBinding!!.ivDishImage)
 
         mBinding!!.tvTitle.text = recipe.title
-        var dishType: String = "other"
+        var dishType = "other"
 
         if(recipe.dishTypes.isNotEmpty()){
             dishType = recipe.dishTypes[0]
             mBinding!!.tvType.text = dishType
         }
 
-        mBinding!!.tvCategory.text = "other"
+        mBinding!!.tvCategory.text = ("other")
         var ingredients = ""
         for (value in recipe.extendedIngredients){
             if(ingredients.isEmpty()){
@@ -110,44 +128,74 @@ class RandomDishFragment : Fragment() {
             mBinding!!.tvCookingDirection.text = Html.fromHtml(recipe.instructions)
         }
 
+        mBinding!!.ivFavoriteDish.setImageDrawable(
+            ContextCompat.getDrawable(
+                requireActivity(),
+                R.drawable.ic_favorite_unselected
+            )
+        )
+
+        var addedToFavorites = false
+
         mBinding!!.tvCookingTime.text =  resources.getString(
             R.string.lbl_estimate_cooking_time,
             recipe.readyInMinutes.toString()
         )
 
         mBinding!!.ivFavoriteDish.setOnClickListener {
-            val randomDishDetails = FavDish(
-                recipe.image,
-                Constants.DISH_IMAGE_SOURCE_ONLINE,
-                recipe.title,
-                dishType,
-                "other",
-                ingredients,
-                recipe.readyInMinutes.toString(),
-                recipe.instructions,
-                true
-            )
-            val mFavDishViewModel: FavDishViewModel by viewModels {
-                FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+
+            if(addedToFavorites){
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_already_added_to_favorites),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }else{
+
+                val randomDishDetails = FavDish(
+                    recipe.image,
+                    Constants.DISH_IMAGE_SOURCE_ONLINE,
+                    recipe.title,
+                    dishType,
+                    "other",
+                    ingredients,
+                    recipe.readyInMinutes.toString(),
+                    recipe.instructions,
+                    true
+                )
+                val mFavDishViewModel: FavDishViewModel by viewModels {
+                    FavDishViewModelFactory((requireActivity().application as FavDishApplication).repository)
+                }
+
+                mFavDishViewModel.insertDish(randomDishDetails)
+
+                addedToFavorites = true
+
+                mBinding!!.ivFavoriteDish.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireActivity(),
+                        R.drawable.ic_favorite_selected
+                    )
+                )
+
+                Toast.makeText(
+                    requireActivity(),
+                    resources.getString(R.string.msg_added_to_favortes),
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
-            mFavDishViewModel.insertDish(randomDishDetails)
-
-            mBinding!!.ivFavoriteDish.setImageDrawable(
-                ContextCompat.getDrawable(
-                    requireActivity(),
-                    R.drawable.ic_favorite_selected
-                )
-            )
-
-            Toast.makeText(
-                requireActivity(),
-                resources.getString(R.string.msg_added_to_favortes),
-                Toast.LENGTH_SHORT
-            ).show()
         }
+    }
 
+    private fun showCustomProgressDialog(){
+        mProgressDialog.let {
+            it.setContentView(R.layout.dialog_custom_progress)
+            it.show()
+        }
+    }
 
-
+    private fun hideProgressDialog(){
+        mProgressDialog.dismiss()
     }
 }
